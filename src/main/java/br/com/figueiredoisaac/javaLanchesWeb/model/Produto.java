30360c2af57720ed74f2bf10cc128ee65d01ff47
Isaac Figueiredo <figueiredoisaac@gmail.com>
2022-11-04 18:13:27 -0300

    Finalização Front-end das paginas de autenticacao

---
+++ src/main/java/br/com/figueiredoisaac/javaLanchesWeb/model/Produto.java
@@ -0,0 +1,78 @@
+package br.com.figueiredoisaac.javaLanchesWeb.model;
+
+import java.math.BigDecimal;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "produtos")
+public class Produto {
+
+	@Id
+	@GeneratedValue (strategy = GenerationType.IDENTITY)
+	private Integer id;
+	private String nome;
+	private String descricao;
+	private BigDecimal valor;
+	
+	
+	public Produto() {
+	
+	}
+
+
+	public Produto(String nome, String descricao, BigDecimal valor) {
+		this.nome = nome;
+		this.descricao = descricao;
+		this.valor = valor;
+	}
+
+
+	public Integer getId() {
+		return id;
+	}
+
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+
+	public String getNome() {
+		return nome;
+	}
+
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+
+	public BigDecimal getValor() {
+		return valor;
+	}
+
+
+	public void setValor(BigDecimal valor) {
+		this.valor = valor;
+	}
+	
+	
+	
+	
+	
+}
