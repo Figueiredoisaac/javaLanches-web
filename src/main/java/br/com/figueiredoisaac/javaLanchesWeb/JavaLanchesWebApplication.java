4dafe7d15ae97764bb953d5bc8cf95c939c61a3e
Isaac Figueiredo <figueiredoisaac@gmail.com>
2022-11-04 05:02:22 -0300

    Git Inicial de Projeto Web com Spring e Maven

---
+++ src/main/java/br/com/figueiredoisaac/javaLanchesWeb/JavaLanchesWebApplication.java
@@ -0,0 +1,13 @@
+package br.com.figueiredoisaac.javaLanchesWeb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaLanchesWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaLanchesWebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/figueiredoisaac/javaLanchesWeb/JavaLanchesWebApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.figueiredoisaac.javaLanchesWeb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaLanchesWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
